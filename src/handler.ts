eb40d7620469409a5483deb136c22035b34e7452
Melanie <mirrorkeydev@gmail.com>
2020-08-15 13:30:27 -0700

    Add temperature endpoint

--- src/handler.ts
+++ src/handler.ts
@@ -5,13 +5,19 @@ import 'source-map-support/register';
 const DB_NAME = "plantdata";
 const COLLECTION_NAME = "plantdata";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const moisture: APIGatewayProxyHandler = async (event, _context) => {
 
   const client = await MongoClient.connect(process.env.URL as string);
   const db = client.db(DB_NAME);
   const coll = db.collection(COLLECTION_NAME);
 
   const result = await coll.aggregate([
+    // Restrict results to the last 3 weeks
+    {
+      "$match": {
+        "meta.time": {"$gte": new Date(new Date().getTime() - (3 * 7 * 24 * 60 * 60 * 1000))}
+      }
+    },
     // Rename our fields and remove unnecessary ones
     { "$project": {
         "datetime": "$meta.time",
@@ -48,7 +54,64 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
         "elinor": { "$push": "$elinor" },
       }
     }
-  ]).limit(100).toArray();
+  ]).toArray();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: result,
+      input: event,
+    }, null, 2),
+  };
+}
+
+export const temperature: APIGatewayProxyHandler = async (event, _context) => {
+
+  const client = await MongoClient.connect(process.env.URL as string);
+  const db = client.db(DB_NAME);
+  const coll = db.collection(COLLECTION_NAME);
+
+  const result = await coll.aggregate([
+    // Restrict results to the last 3 weeks
+    {
+      "$match": {
+        "meta.time": {"$gte": new Date(new Date().getTime() - (3 * 7 * 24 * 60 * 60 * 1000))}
+      }
+    },
+    // Rename our fields and remove unnecessary ones
+    { "$project": {
+        "datetime": "$meta.time",
+        "temp": "$data.climate.tempurature",
+      }
+    },
+    // Group by buckets of 2 hours
+    { "$group": {
+        "_id": {
+          "$floor": {
+            "$divide": [
+              { "$toLong": "$datetime" },
+              7.2e6 // 2 hours in milliseconds
+            ]
+          }
+        },
+        "datetime": { "$first": "$datetime" },
+        "temp": { "$avg": "$temp" },
+      }
+    },
+    // Sort chronologically
+    {
+      "$sort": {
+        "datetime": 1
+      }
+    },
+    // Change output to be separate arrays
+    { "$group": {
+        "_id": 1,
+        "datetime": { "$push": "$datetime" },
+        "temp": { "$push": "$temp" },
+      }
+    }
+  ]).toArray();
 
   return {
     statusCode: 200,
