51169df16880965932e8d89815b177116796a4dd
mirrorkeydev <mirrorkeydev@gmail.com>
2021-03-29 17:13:43 -0700

    Add humidity and fix dependencies

--- src/handler.ts
+++ src/handler.ts
@@ -7,77 +7,83 @@ const COLLECTION_NAME = "plantdata";
 
 let client: MongoClient | null = null;
 
-export const plantdata: APIGatewayProxyHandler = async (_event, context) => {
-  // See https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
-  context.callbackWaitsForEmptyEventLoop = false
-  if (client === null || !client.isConnected()) {
-    client = await MongoClient.connect(process.env.URL as string);
-  }
-  // access plant data
-  const db = client.db(DB_NAME);
-  const coll = db.collection(COLLECTION_NAME);
-
-  const result = await coll.aggregate([
-    // Restrict results to the last 3 weeks
-    {
-      "$match": {
-        "meta.time": {"$gte": new Date(new Date().getTime() - (3 * 7 * 24 * 60 * 60 * 1000))}
-      }
-    },
-    // Rename our fields and remove unnecessary ones
-    { "$project": {
-        "datetime": "$meta.time",
-        "ophelia": "$data.soil.0x26",
-        "elinor": "$data.soil.0x24",
-        "temp": "$data.climate.tempurature",
-        "pressure": "$data.climate.pressure",
-        "light": "$data.lux",
-      }
-    },
-    // Group by buckets of 2 hours
-    { "$group": {
-        "_id": {
-          "$floor": {
-            "$divide": [
-              { "$toLong": "$datetime" },
-              7.2e6 // 2 hours in milliseconds
-            ]
-          }
-        },
-        "datetime": { "$first": "$datetime" },
-        "ophelia": { "$avg": "$ophelia" },
-        "elinor": { "$avg": "$elinor" },
-        "temp": { "$avg": "$temp" },
-        "pressure": { "$avg": "$pressure" },
-        "light": { "$avg": "$light" },
-      }
-    },
-    // Sort chronologically
-    {
-      "$sort": {
-        "datetime": 1
-      }
-    },
-    // Change output to be separate arrays
-    { "$group": {
-        "_id": 1,
-        "datetime": { "$push": "$datetime" },
-        "ophelia": { "$push": "$ophelia" },
-        "elinor": { "$push": "$elinor" },
-        "temp": { "$push": "$temp" },
-        "pressure": { "$push": "$pressure" },
-        "light": { "$push": "$light" },
-      }
+export const plantdata: APIGatewayProxyHandler = (_event, context, callback) => {
+  (async () => {
+    // See https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
+    context.callbackWaitsForEmptyEventLoop = false
+    if (client === null || !client.isConnected()) {
+      client = await MongoClient.connect(process.env.URL as string, { useUnifiedTopology: true });
     }
-  ]).toArray();
+    // access plant data
+    const db = client.db(DB_NAME);
+    const coll = db.collection(COLLECTION_NAME);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: result,
-    }),
-    headers: {
-      'Access-Control-Allow-Origin': 'https://mirrorkey.dev',
-    },
-  };
+    const result = await coll.aggregate([
+      // Restrict results to the last 3 weeks
+      {
+        "$match": {
+          "meta.time": {"$gte": new Date(new Date().getTime() - (3 * 7 * 24 * 60 * 60 * 1000))}
+        }
+      },
+      // Rename our fields and remove unnecessary ones
+      { "$project": {
+          "datetime": "$meta.time",
+          "ophelia": "$data.soil.0x26",
+          "elinor": "$data.soil.0x24",
+          "temp": "$data.climate.tempurature",
+          "pressure": "$data.climate.pressure",
+          "humidity": "$data.humidity",
+          "light": "$data.lux",
+        }
+      },
+      // Group by buckets of 2 hours
+      { "$group": {
+          "_id": {
+            "$floor": {
+              "$divide": [
+                { "$toLong": "$datetime" },
+                7.2e6 // 2 hours in milliseconds
+              ]
+            }
+          },
+          "datetime": { "$first": "$datetime" },
+          "ophelia": { "$avg": "$ophelia" },
+          "elinor": { "$avg": "$elinor" },
+          "temp": { "$avg": "$temp" },
+          "pressure": { "$avg": "$pressure" },
+          "humidity": { "$avg" : "$humidity" },
+          "light": { "$avg": "$light" },
+        }
+      },
+      // Sort chronologically
+      {
+        "$sort": {
+          "datetime": 1
+        }
+      },
+      // Change output to be separate arrays
+      { "$group": {
+          "_id": 1,
+          "datetime": { "$push": "$datetime" },
+          "ophelia": { "$push": "$ophelia" },
+          "elinor": { "$push": "$elinor" },
+          "temp": { "$push": "$temp" },
+          "pressure": { "$push": "$pressure" },
+          "humidity": { "$push": "$humidity" },
+          "light": { "$push": "$light" },
+        }
+      }
+    ]).toArray();
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: result,
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': 'https://mirrorkey.dev',
+      },
+    });
+    
+  })()
 }
